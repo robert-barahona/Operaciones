4d703d033f30f435ff8603131286bb8892fbd933
Roberto Barahona <rabarahona1@espe.edu.ec>
2020-04-15 18:48:19 -0500

    Added bottom navigation bar with 3 items

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:operaciones/my_custom_icons_icons.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,9 +15,26 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey,
-        
-      ),
+          backgroundColor: Color.fromARGB(255, 44, 62, 80),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Color.fromARGB(255, 23, 32, 42),
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Colors.cyan,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(MyCustomIcons.plus),
+                title: Text('Suma'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(MyCustomIcons.minus),
+                title: Text('Resta'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(MyCustomIcons.multiplication),
+                title: Text('Multiplicación'),
+              )
+            ],
+          )),
     );
   }
 }
